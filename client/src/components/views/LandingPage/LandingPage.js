8491049814843450532b5df7d02213c030386d63
Sanki0 <sebastian.ferreyra.c@uni.pe>
2021-12-27 01:45:49 -0500

    funciona

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <>
+            <div className="app">
+                <h1 className="Home-title">CRC cards Generator</h1>
+                <div className="Home-descripcion">
+                    aqui va una descripcion del proyecto
+                </div>
+                <div className="mision">
+                    <h2>Mision</h2>
+                    aqui va la mision
+                </div>
+                <div className="vision">
+                    <h2>Vision</h2>
+                    aqui va la vision
+                </div>
+            </div>
+            
+        </>
+    )
+}
+
+export default LandingPage
